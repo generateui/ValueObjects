91b5c31648465744ebd90f29a52fcdb1b296251c
Ruud Poutsma <r.poutsma@evidos.nl>
2023-12-15 17:01:59 +0100

    Improve PageNumber

--- src/ValueObjects/PageNumber.cs
+++ src/ValueObjects/PageNumber.cs
@@ -3,7 +3,9 @@ namespace ValueObjects;
 /// <summary>
 /// Implementation of https://en.wikipedia.org/wiki/Page_numbering
 /// </summary>
-public sealed class PageNumber {
+public sealed class PageNumber :
+	IEquatable<PageNumber>,
+	IComparable<PortNumber> {
 	public PageNumber(int number) {
 		if (number < 1 ) {
 			throw new ArgumentException(
@@ -14,4 +16,29 @@ public sealed class PageNumber {
 	}
 
 	public int Value { get; }
+
+	public int CompareTo(PortNumber? other) {
+		if (other is null) {
+			return 1;
+		}
+		return Value.CompareTo(other.Value);
+	}
+
+	public override bool Equals(object? obj) {
+		if (obj is not PortNumber portNumber) {
+			return false;
+		}
+		return Equals(Value, portNumber.Value);
+	}
+
+	public bool Equals(PageNumber? other) {
+		if (other is null) {
+			return false;
+		}
+		return Equals(Value, other.Value);
+	}
+
+	public override int GetHashCode() => Value.GetHashCode();
+
+	public override string ToString() => Value.ToString();
 }
